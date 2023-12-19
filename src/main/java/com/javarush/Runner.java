6ba4a42b63999f9620739336f647a886f46d9cb2
digital-hot <grey.egorov@gmail.com>
2023-12-19 10:48:15 +0200

    Реалізація класу RunnerService

--- src/main/java/com/javarush/Runner.java
+++ src/main/java/com/javarush/Runner.java
@@ -3,38 +3,14 @@ package com.javarush;
 import java.util.Scanner;
 
 class Runner {
-
     private static int MAGIC_NUMBER = 3;
 
-
     public static void main(String[] args) {
+        RunnerService runnerService = new RunnerService();
         if (args.length == MAGIC_NUMBER) {
-            runWithArguments(args);
+            runnerService.runWithArguments(args);
         } else {
-            runWithCommandLine();
+            runnerService.runWithCommandLine();
         }
-    }
-
-    private static void runWithArguments(String[] args) {
-        CLI.processArguments(args);
-    }
-
-    private static void runWithCommandLine() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter command (ENCRYPT, DECRYPT, BRUTE_FORCE):");
-        String command = scanner.nextLine().toUpperCase();
-
-        System.out.println("Enter file path:");
-        String filePath = scanner.nextLine();
-
-        int key = 0;
-        if (!command.equals("BRUTE_FORCE")) {
-            System.out.println("Enter key:");
-            key = scanner.nextInt();
-        }
-
-        String[] arguments = {command, filePath, String.valueOf(key)};
-        CLI.processArguments(arguments);
     }
 }
