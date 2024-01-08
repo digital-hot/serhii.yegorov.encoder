4487517438dab092e807a7cbfee84cbb5604541c
digital-hot <grey.egorov@gmail.com>
2024-01-08 11:52:17 +0200

    Виправлена назва константи

--- src/main/java/com/javarush/Runner.java
+++ src/main/java/com/javarush/Runner.java
@@ -1,10 +1,10 @@
 package com.javarush;
 
 class Runner {
-    static final int MAGIC_NUMBER = 3;
+    static final int ARGUMENTS_SIZE = 3;
     public static void main(String[] args) {
         RunnerService runnerService = new RunnerService();
-        if (args.length == MAGIC_NUMBER) {
+        if (args.length == ARGUMENTS_SIZE) {
             runnerService.runWithArguments(args);
         } else {
             runnerService.runWithCommandLine();
